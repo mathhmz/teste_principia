448e004fa2f162763196aff06ad9e98a4fefea33
mathhmz <rm551843@fiap.com.br>
2024-09-06 16:29:41 +0000

    feat: Atualizando resolucao.md e documentando classes

--- arquivo.py
+++ arquivo.py
@@ -11,15 +11,28 @@ dados = "dados.xlsx"
 sistema = "sistema.xlsx"
 
 class DataService: 
+    """
+    Classe responsável por carregar os dados de duas fontes (arquivos Excel).
+    
+    Atributos:
+        dados_path (str): O caminho do arquivo de dados dos clientes.
+        sistema_path (str): O caminho do arquivo contendo os clientes já cadastrados no sistema.
+    """
+    
     def __init__(self, dados_path, sistema_path) -> None: 
         self.dados_path =  dados_path
         self.sistema_path = sistema_path
         
     def get_data(self) -> list:
+        """
+        Lê os arquivos de dados e do sistema e retorna uma lista de DataFrames.
         
+        Retorna:
+            list: Uma lista contendo dois DataFrames, um com os dados dos clientes e outro com os clientes já cadastrados.
+        """
         dfs = []
-        
         sources = [self.dados_path, self.sistema_path]
+        
         for source in sources:
             file_name, file_extension = os.path.splitext(source)
             
@@ -36,13 +49,32 @@ class DataService:
         return dfs
     
     def __call__(self) -> list:
+        """
+        Método que torna a instância da classe chamável para obter os dados.
+        
+        Retorna:
+            list: A lista de DataFrames retornada por `get_data()`.
+        """
         return self.get_data()
     
 class RowValidator:
+    """
+    Classe responsável por validar os dados de cada linha de cliente.
+    
+    Atributos:
+        data (dict): Dicionário contendo os dados do cliente a ser validado.
+    """
+    
     def __init__(self, data):
         self.data = data
         
-    def validate_cpf(self):
+    def validate_cpf(self) -> bool:
+        """
+        Valida o CPF do cliente usando as regras de verificação de dígitos e formato.
+
+        Retorna:
+            bool: True se o CPF for válido, False caso contrário.
+        """
         cpf = self.data.get('cpf')
         cpf = re.sub(r'\D', '', cpf)
         if len(cpf) != 11 or cpf == cpf[0] * 11:
@@ -54,11 +86,23 @@ class RowValidator:
                 return False
         return True
 
-    def validate_name(self):
+    def validate_name(self) -> bool:
+        """
+        Valida se o nome do cliente tem pelo menos dois nomes.
+
+        Retorna:
+            bool: True se o nome for válido, False caso contrário.
+        """
         name = self.data.get('nome')
         return len(name.split()) >= 2
 
-    def validate_birthday_and_age(self):
+    def validate_birthday_and_age(self) -> bool:
+        """
+        Valida a data de nascimento e a idade do cliente (deve ser maior ou igual a 17 anos).
+
+        Retorna:
+            bool: True se o cliente tiver 17 anos ou mais e a data for válida, False caso contrário.
+        """
         birthday = self.data.get('data_nasc')
         today = datetime.today()
         
@@ -80,17 +124,35 @@ class RowValidator:
         age = (today - birth_date).days / 365.25
         return age >= 17
 
-    def validate_email(self):
+    def validate_email(self) -> bool:
+        """
+        Valida o email do cliente.
+
+        Retorna:
+            bool: True se o email for válido, False caso contrário.
+        """
         email = self.data.get('email', "")
         regex = r'^\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
         return re.match(regex, email) is not None
 
-    def validate_phone(self):
+    def validate_phone(self) -> bool:
+        """
+        Valida o número de telefone celular do cliente.
+
+        Retorna:
+            bool: True se o telefone for válido, False caso contrário.
+        """
         phone_number = self.data.get('celular')
         regex = r'^\([1-9]{2}\) (?:[2-8]|9[0-9])[0-9]{3}\-[0-9]{4}$'  
         return re.match(regex, phone_number) is not None
 
-    def validate_cep(self):
+    def validate_cep(self) -> tuple:
+        """
+        Valida o CEP do cliente usando a API ViaCEP.
+
+        Retorna:
+            tuple: Um booleano indicando se o CEP é válido e, se válido, um dicionário com os dados do endereço.
+        """
         cep = self.data.get('cep')
         url = f"https://viacep.com.br/ws/{cep}/json/"
         response = requests.get(url)
@@ -100,7 +162,13 @@ class RowValidator:
                 return True, dados_cep
         return False, None
 
-    def validar_cliente(self):
+    def validar_cliente(self) -> dict:
+        """
+        Realiza todas as validações de cliente.
+
+        Retorna:
+            dict: Um dicionário com os resultados de cada validação.
+        """
         resultado = {
             'cpf_validation': self.validate_cpf(),
             'name_validation': self.validate_name(),
@@ -111,38 +179,72 @@ class RowValidator:
         }
         return resultado
 
-    def pipeline(self):
+    def pipeline(self) -> dict:
+        """
+        Executa o pipeline de validação do cliente.
+
+        Retorna:
+            dict: O resultado das validações.
+        """
         validacoes = self.validar_cliente()
         return validacoes
 
-    def __call__(self): 
+    def __call__(self) -> dict: 
+        """
+        Torna a instância da classe chamável para realizar a validação do cliente.
+
+        Retorna:
+            dict: O resultado das validações do pipeline.
+        """
         return self.pipeline()
 
 class ValidatorService:
-    def __init__(self,dataframe) -> None:
+    """
+    Classe responsável por normalizar os dados e validar os clientes.
+
+    Atributos:
+        dataframe (pd.DataFrame): O DataFrame contendo os dados dos clientes.
+    """
+    
+    def __init__(self, dataframe) -> None:
         self.dataframe = dataframe
     
     def normalize_dataframe(self) -> pd.DataFrame:
-        columns = {"NOME" : "nome",
-            "CPF" : "cpf",
-            "Data de Nascimento" : "data_nasc",
-            "Email" : "email",
-            "CEP" : "cep",
-            "Endereço" : "endereco",
-            "Numero" : "numero",
-            "Bairro" : "bairro",
-            "Cidade" : "cidade",
-            "Estado" : "estado",
-            "Telefone" : "celular",
-            "RA" : "ra",
-            "Curso" : "curso",
-            "Faculdade" : "faculdade"
-            }
-            
-        normalized_dataframe = self.dataframe.rename(columns = columns)
+        """
+        Normaliza o DataFrame renomeando as colunas de acordo com o formato esperado.
+
+        Retorna:
+            pd.DataFrame: O DataFrame normalizado.
+        """
+        columns = {
+            "NOME": "nome",
+            "CPF": "cpf",
+            "Data de Nascimento": "data_nasc",
+            "Email": "email",
+            "CEP": "cep",
+            "Endereço": "endereco",
+            "Numero": "numero",
+            "Bairro": "bairro",
+            "Cidade": "cidade",
+            "Estado": "estado",
+            "Telefone": "celular",
+            "RA": "ra",
+            "Curso": "curso",
+            "Faculdade": "faculdade"
+        }
+        normalized_dataframe = self.dataframe.rename(columns=columns)
         return normalized_dataframe
     
     def create_valid_and_detached_list(self, dataframe: pd.DataFrame) -> Any:
+        """
+        Valida as linhas do DataFrame e cria listas de clientes válidos e rejeitados.
+
+        Args:
+            dataframe (pd.DataFrame): O DataFrame a ser validado.
+
+        Retorna:
+            list: Duas listas, uma com os clientes válidos e outra com os rejeitados.
+        """
         valid_list = []
         detached_list = []
 
@@ -150,41 +252,58 @@ class ValidatorService:
             l = line.to_dict()
             
             row_validator = RowValidator(l)
-            
             is_valid = row_validator()
             
             if all(condition == True for condition in is_valid.values()):
                 valid_list.append(l)
-
             else: 
                 detach_reasons = [key for key, value in is_valid.items() if not value]
                 l["detach_reason"] = detach_reasons
                 detached_list.append(l)
                 
-        print(valid_list)
-        print(detached_list)
-                
         complete_lists = [valid_list, detached_list]
-                
         return complete_lists
     
     def pipeline(self) -> Any:
+        """
+        Executa o pipeline de normalização e validação.
+
+        Retorna:
+            Any: Duas listas de clientes, uma válida e outra rejeitada.
+        """
         df = self.normalize_dataframe()
-        complete_lists = self.create_valid_and_detached_list(dataframe= df)
-        print(complete_lists)
+        complete_lists = self.create_valid_and_detached_list(dataframe=df)
         return complete_lists
     
     def __call__(self) -> Any:
+        """
+        Torna a instância da classe chamável para executar o pipeline.
+
+        Retorna:
+            Any: Duas listas de clientes, uma válida e outra rejeitada.
+        """
         return self.pipeline()
-        
+
 class UpdateOrDuplicatedChecker:
-    def __init__(self, lists : list, df_system : pd.DataFrame) -> None:
+    """
+    Classe responsável por verificar se há necessidade de atualização ou duplicação de clientes no sistema.
+
+    Atributos:
+        lists (list): Duas listas de clientes, uma válida e outra rejeitada.
+        df_system (pd.DataFrame): O DataFrame contendo os clientes já cadastrados no sistema.
+    """
+    
+    def __init__(self, lists: list, df_system: pd.DataFrame) -> None:
         self.valid, self.detached = lists
         self.df_system = df_system
     
     def check_update_needed(self):
-        print(self.valid)
-        print(self.detached)
+        """
+        Verifica se os clientes válidos precisam ser atualizados no sistema ou inseridos como novos.
+
+        Retorna:
+            list: Dois DataFrames, um com os clientes válidos e outro com os rejeitados após a verificação de duplicação.
+        """
         df_detached = pd.DataFrame(self.detached)
         df_valid = pd.DataFrame(self.valid)
         df_system = self.df_system.copy()
@@ -192,44 +311,49 @@ class UpdateOrDuplicatedChecker:
         df_system['cpf'] = df_system['cpf'].str.replace(r'\D', '', regex=True)
         
         common_cpf = set(df_valid['cpf']).intersection(set(df_system['cpf']))
-        
         removed_from_valid = []
         
         for cpf in common_cpf:
-            df_valid_row = df_valid[df_valid['cpf'] == cpf].drop(columns='cpf').reset_index(drop = True)
-            df_system_row = df_system[df_system['cpf'] == cpf].drop(columns='cpf').reset_index(drop = True)
+            df_valid_row = df_valid[df_valid['cpf'] == cpf].drop(columns='cpf').reset_index(drop=True)
+            df_system_row = df_system[df_system['cpf'] == cpf].drop(columns='cpf').reset_index(drop=True)
             
             common_cols = df_valid_row.columns.intersection(df_system_row.columns)
-            
-            print(common_cols)
-            
             df_valid_row_common = df_valid_row[common_cols]
             df_system_row_common = df_system_row[common_cols]
-            
-            print(df_valid_row_common)
-            print(df_system_row_common)
             
             if df_valid_row_common.equals(df_system_row_common):
                 valid_row_dict = df_valid[df_valid['cpf'] == cpf].to_dict('records')[0]
                 valid_row_dict['detach_reason'] = ['update_validation']
                 removed_from_valid.append(valid_row_dict)
                 df_valid = df_valid[df_valid['cpf'] != cpf]
-
             else:
                 df_valid.loc[df_valid['cpf'] == cpf, 'tipo'] = 'A'
                     
         df_detached = pd.concat([df_detached, pd.DataFrame(removed_from_valid)], ignore_index=True)
-                
         df_valid.loc[~df_valid['cpf'].isin(df_system['cpf']), 'tipo'] = 'I'
-        
         df_detached.reset_index(drop=True, inplace=True)
         
         return [df_valid, df_detached]
     
     def __call__(self) -> Any:
+        """
+        Torna a instância da classe chamável para verificar a necessidade de atualização ou inserção.
+
+        Retorna:
+            Any: Dois DataFrames, um com os clientes válidos e outro com os rejeitados.
+        """
         return self.check_update_needed()
-        
+
 def transformar_em_json(df):
+    """
+    Transforma um DataFrame em uma lista de objetos JSON para upload no sistema.
+
+    Args:
+        df (pd.DataFrame): O DataFrame a ser convertido.
+
+    Retorna:
+        list: Uma lista de dicionários representando os clientes em formato JSON.
+    """
     json_list = []
     for _, row in df.iterrows():
         agrupador = row['faculdade'].lower()  
@@ -294,6 +418,7 @@ def transformar_em_json(df):
         json_list.append(json_obj)
     return json_list
 
+# Execução do pipeline
 get_data = DataService(dados,sistema)
 data = get_data()
 
--- resolucao.md
+++ resolucao.md
@@ -1,44 +1,48 @@
-Resolução do Problema: Validação e Inserção de Clientes no Sistema
-Contexto
+
+
+# Resolução do Problema: Validação e Inserção de Clientes no Sistema
+
+## Contexto
 Este exercício envolve a inserção de novos clientes em um sistema a partir de um arquivo JSON. Antes de realizar a inserção, várias validações precisam ser feitas, incluindo CPF, nome completo, data de nascimento (verificação de idade mínima), email, telefone e CEP. Também é necessário verificar se o cliente já existe no sistema e, caso positivo, atualizar suas informações. Ao final, o resultado esperado inclui um arquivo JSON para os clientes aprovados, um relatório em Excel com os clientes rejeitados e suas razões, e o código em Python para realizar esse processo.
 
-Etapas de Resolução
-1. Leitura dos Dados
-Utilizamos a classe DataService para ler dois arquivos principais:
+## Etapas de Resolução
 
-dados.xlsx: contendo as informações dos novos clientes.
-sistema.xlsx: contendo os clientes já cadastrados no sistema.
-A função get_data() lê ambos os arquivos e retorna dois DataFrames, um para cada fonte de dados.
+### 1. Leitura dos Dados
+Utilizamos a classe `DataService` para ler dois arquivos principais:
+- `dados.xlsx`: contendo as informações dos novos clientes.
+- `sistema.xlsx`: contendo os clientes já cadastrados no sistema.
 
-2. Validação dos Dados dos Clientes
-A classe RowValidator foi implementada para realizar as validações de cada linha no DataFrame de clientes. Essas validações incluem:
+A função `get_data()` lê ambos os arquivos e retorna dois DataFrames, um para cada fonte de dados.
 
-CPF: Usamos expressões regulares para validar o formato e aplicamos a lógica de verificação de dígitos.
-Nome: Verificamos se o cliente possui nome completo (dois ou mais nomes).
-Data de Nascimento: Garantimos que a data de nascimento esteja correta e que o cliente tenha ao menos 17 anos.
-Email: Verificamos o formato de email com uma expressão regular.
-Telefone: Validamos o formato de telefone com DDD no padrão brasileiro.
-CEP: Utilizamos a API do ViaCEP para verificar a existência do CEP e validar o endereço.
-3. Normalização dos Dados
-A classe ValidatorService foi responsável por normalizar as colunas do DataFrame de acordo com as necessidades do sistema, renomeando as colunas para os formatos exigidos. Essa etapa é crucial para manter a consistência e garantir que as validações e comparações sejam feitas corretamente.
+### 2. Validação dos Dados dos Clientes
+A classe `RowValidator` foi implementada para realizar as validações de cada linha no DataFrame de clientes. Essas validações incluem:
+- **CPF**: Usamos expressões regulares para validar o formato e aplicamos a lógica de verificação de dígitos.
+- **Nome**: Verificamos se o cliente possui nome completo (dois ou mais nomes).
+- **Data de Nascimento**: Garantimos que a data de nascimento esteja correta e que o cliente tenha ao menos 17 anos.
+- **Email**: Verificamos o formato de email com uma expressão regular.
+- **Telefone**: Validamos o formato de telefone com DDD no padrão brasileiro.
+- **CEP**: Utilizamos a API do ViaCEP para verificar a existência do CEP e validar o endereço.
 
-4. Geração de Listas de Clientes Validados e Rejeitados
-Após a normalização, a função create_valid_and_detached_list() percorre o DataFrame de clientes. Ela valida cada cliente individualmente utilizando a RowValidator e separa os clientes em duas listas:
+### 3. Normalização dos Dados
+A classe `ValidatorService` foi responsável por normalizar as colunas do DataFrame de acordo com as necessidades do sistema, renomeando as colunas para os formatos exigidos. Essa etapa é crucial para manter a consistência e garantir que as validações e comparações sejam feitas corretamente.
 
-valid_list: Clientes que passaram em todas as validações.
-detached_list: Clientes que falharam em alguma validação. Além disso, cada cliente rejeitado recebe um campo adicional chamado detach_reason, contendo a lista de razões pelas quais a validação falhou.
-5. Verificação de Duplicidade ou Atualização
-A classe UpdateOrDuplicatedChecker realiza a verificação entre os clientes validados e os clientes já cadastrados no sistema. Ela faz isso comparando o CPF dos clientes. Quando um cliente já existe no sistema:
+### 4. Geração de Listas de Clientes Validados e Rejeitados
+Após a normalização, a função `create_valid_and_detached_list()` percorre o DataFrame de clientes. Ela valida cada cliente individualmente utilizando a `RowValidator` e separa os clientes em duas listas:
+- **valid_list**: Clientes que passaram em todas as validações.
+- **detached_list**: Clientes que falharam em alguma validação. Além disso, cada cliente rejeitado recebe um campo adicional chamado `detach_reason`, contendo a lista de razões pelas quais a validação falhou.
 
-Se seus dados são idênticos, ele é movido para a lista de clientes rejeitados com a razão "update_validation".
-Se há diferenças, o cliente é marcado para atualização com o tipo "A" (atualização). Clientes não existentes são classificados com o tipo "I" (inserção).
-6. Transformação dos Dados em JSON
-A função transformar_em_json() transforma os clientes validados (tanto os de inserção quanto os de atualização) em objetos JSON no formato exigido pelo sistema. Esta função constrói um dicionário com as informações pessoais, endereços, emails e telefones dos clientes, pronto para ser inserido no sistema.
+### 5. Verificação de Duplicidade ou Atualização
+A classe `UpdateOrDuplicatedChecker` realiza a verificação entre os clientes validados e os clientes já cadastrados no sistema. Ela faz isso comparando o CPF dos clientes. Quando um cliente já existe no sistema:
+- Se seus dados são idênticos, ele é movido para a lista de clientes rejeitados com a razão "update_validation".
+- Se há diferenças, o cliente é marcado para atualização com o tipo "A" (atualização). Clientes não existentes são classificados com o tipo "I" (inserção).
 
-7. Geração de Arquivos Finais
+### 6. Transformação dos Dados em JSON
+A função `transformar_em_json()` transforma os clientes validados (tanto os de inserção quanto os de atualização) em objetos JSON no formato exigido pelo sistema. Esta função constrói um dicionário com as informações pessoais, endereços, emails e telefones dos clientes, pronto para ser inserido no sistema.
+
+### 7. Geração de Arquivos Finais
 Ao final do processo, o código gera dois arquivos:
+- Um arquivo `JSON` com os clientes validados e prontos para serem inseridos ou atualizados no sistema.
+- Um arquivo `Excel` (`dados_descartados-{data}.xlsx`) com os clientes rejeitados e as razões pelas quais não puderam ser aceitos.
 
-Um arquivo JSON (dados-{data}.json)com os clientes validados e prontos para serem inseridos ou atualizados no sistema.
-Um arquivo Excel (dados_descartados-{data}.xlsx) com os clientes rejeitados e as razões pelas quais não puderam ser aceitos.
-Considerações Finais
+### Considerações Finais
 O código foi escrito para ser modular e de fácil manutenção. As classes responsáveis por cada parte do processo são independentes, o que facilita modificações futuras ou a introdução de novas regras de negócio. Cada validação foi cuidadosamente implementada para garantir a conformidade com as regras fornecidas, assegurando a precisão dos dados antes de subir ao sistema.
